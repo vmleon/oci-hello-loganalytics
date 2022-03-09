434b8581771377bd39cd7ca0b168c7aa5a11edc8
Victor Martin <victor.martin.alvarez@oracle.com>
2022-03-09 10:04:37 +0000

    Fix helm_release dependency issue

--- provisioning/helm_hello_api.tf
+++ provisioning/helm_hello_api.tf
@@ -1,4 +1,6 @@
 resource "helm_release" "hello-api" {
   name       = "hello-api"
   chart      = "../helm-charts/hello-api"
+
+  depends_on = [data.oci_containerengine_cluster_kube_config.oke, local_file.oke_kubeconfig]
 }
--- provisioning/provider.tf
+++ provisioning/provider.tf
@@ -25,31 +25,8 @@ provider "oci" {
   private_key_path = var.private_key_path
 }
 
-provider "kubernetes" {
-  host                   = local.cluster_endpoint
-  cluster_ca_certificate = local.cluster_ca_certificate
-  exec {
-    api_version = "client.authentication.k8s.io/v1beta1"
-    args        = ["ce", "cluster", "generate-token", "--cluster-id", local.cluster_id, "--region", local.cluster_region]
-    command     = "oci"
-  }
-}
-
 provider "helm" {
   kubernetes {
-    host                   = local.cluster_endpoint
-    cluster_ca_certificate = local.cluster_ca_certificate
-    exec {
-      api_version = "client.authentication.k8s.io/v1beta1"
-      args        = ["ce", "cluster", "generate-token", "--cluster-id", local.cluster_id, "--region", local.cluster_region]
-      command     = "oci"
-    }
+    config_path = "${path.module}/generated/kubeconfig"
   }
-}
-
-locals {
-  cluster_endpoint       = yamldecode(data.oci_containerengine_cluster_kube_config.oke.content)["clusters"][0]["cluster"]["server"]
-  cluster_ca_certificate = base64decode(yamldecode(data.oci_containerengine_cluster_kube_config.oke.content)["clusters"][0]["cluster"]["certificate-authority-data"])
-  cluster_id             = yamldecode(data.oci_containerengine_cluster_kube_config.oke.content)["users"][0]["user"]["exec"]["args"][4]
-  cluster_region         = yamldecode(data.oci_containerengine_cluster_kube_config.oke.content)["users"][0]["user"]["exec"]["args"][6]
 }
